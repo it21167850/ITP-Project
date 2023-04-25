e7fc9a41dd6c7629c2d82ca8255be4eda8d5f836
IT21211300 <IT21211300@my.sliit.lk>
2023-04-25 18:22:15 +0530

    update ownmeal-controller and server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 const Menuownroutes = require("./routes/Ownmeal-routes");
 const Payment = require("./routes/payment-routes");
+const routesCreOWME = require("./routes/Ownmeal-routes");
 // const OrderForm = require("./routes/OrderForm-routes");
 // const router3 = require("./routes/OrderForm-routes");
 
@@ -22,6 +23,7 @@ app.use("/fooditems", router);
 app.use("/packages", router1);
 app.use("/customerfooditems", router);
 app.use("/customerpackages", router1);
+app.use("/menudash/CustOwnMeal",routesCreOWME);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/custownmeal", Menuownroutes);
