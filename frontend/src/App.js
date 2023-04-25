ded9031499c11eff54e49125343af32786fdeb09
IT21211300 <IT21211300@my.sliit.lk>
2023-04-25 14:44:55 +0530

    Custownmeal page
    that page is uploaded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import AdminDash from "./components/Dashboard/AdminDash/AdminDash";
 import MenuDash from "./components/Dashboard/MenuDash/MenuDash";
 import Delivery from "./components/Dashboard/DeliveryDash/DeliveryDash";
 import Dtable from "./pages/delivery/DeliveryTable/Dtable";
+import CustOwnMeal from "./pages/Menu/CustOwnMeal";
 function App() {
   const user = localStorage.getItem("token");
   return (
@@ -23,6 +24,7 @@ function App() {
         <Route path="/empdash" element={<Navigate replace to="/login" />} />
         <Route path="/admindash" element={<Navigate replace to="/login" />} />
         <Route path="/deliverydash/dtable" element={<Dtable />} exact />
+        <Route path="/menudash" element={<CustOwnMeal />} exact />
       </Routes>
     </>
   );
