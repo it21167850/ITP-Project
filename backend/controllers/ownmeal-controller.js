261ee1334706e916cedb36ff72b4297a108b6f19
IT21211300 <IT21211300@my.sliit.lk>
2023-05-04 13:44:48 +0530

    Update ownmeal-controller.js

--- backend/controllers/ownmeal-controller.js
+++ backend/controllers/ownmeal-controller.js
@@ -8,9 +8,9 @@ const ownmeal = require('../models/Ownmeal')
 
 
 const getAllownmeals = async(req,res,next) =>{
-    let fooditems;
+    let omeal;
     try{
-        fooditems= await ownmeal.find();
+        omeal= await ownmeal.find();
 
     }
     catch(err)
@@ -18,11 +18,11 @@ const getAllownmeals = async(req,res,next) =>{
         console.log(err);
     }
 
-    if(!fooditems)
+    if(!omeal)
     {
         return res.status(404).json({message:"No product found"})
     }
-    return res.status(200).json({fooditems})
+    return res.status(200).json({omeal})
 
 }
 
