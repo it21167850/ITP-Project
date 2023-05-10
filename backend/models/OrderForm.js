126c7fa45030d1cca87a3391947c1d169ccb1ecf
it21387494samitha <IT21387494@my.sliit.lk>
2023-05-10 22:25:57 +0530

    up

--- backend/models/OrderForm.js
+++ backend/models/OrderForm.js
@@ -5,32 +5,25 @@ const Schema = mongoose.Schema;
 const addorderdetails = new Schema({
   total: {
     type: String,
-    require: true,
   },
   orderedfood: {
     type: String,
-    require: true,
   },
   qty: {
     type: String,
-    require: true,
   },
 
   name: {
     type: String,
-    require: true,
   },
   Address: {
     type: String,
-    require: true,
   },
   Phone: {
     type: Number,
-    require: true,
   },
   email: {
     type: String,
-    require: true,
   },
 });
 
